878b611179e194e7a5ffc1682499306c023d0809
Papaya Delight <jasmineteters2020@gmail.com>
2021-02-20 12:15:55 -0700

    made responsive navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Home from './pages/home';
+// import Seller from './pages/seller';
+// import Signin from './Pages/SignupLogin'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+
+      </Switch>
+
+    </Router>
+    </>
   );
 }
 
