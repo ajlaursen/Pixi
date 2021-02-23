07c29e4ed49d5395c0a352cc5b4c262f049e97b3
Papaya Delight <jasmineteters2020@gmail.com>
2021-02-22 21:27:13 -0700

    adjusted the card formatting, made responsive

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Card from './Components/Cards/card'
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
+import card from './Components/Cards/card';
 // import Home from './pages/home';
 // import Seller from './pages/seller';
 // import Signin from './Pages/SignupLogin'
@@ -13,16 +14,20 @@ import Signup from './Components/Signup';
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Card />
-    <Login/>
-    <Signup/>
-      <Switch>
+      <Router>
+        <Navbar />
+        <div className="flex flex-col tablet:flex-row justify-center flex-wrap">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
 
-      </Switch>
 
-    </Router>
+        <Switch></Switch>
+      </Router>
     </>
   );
 }
