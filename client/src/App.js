cd5401ff42f9351cca003c19fa6e17e224816506
Papaya Delight <jasmineteters2020@gmail.com>
2021-02-22 21:13:13 -0700

    created the login and signup components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './Components/Navbar';
 import Card from './Components/Cards/card'
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 // import Home from './pages/home';
 // import Seller from './pages/seller';
 // import Signin from './Pages/SignupLogin'
@@ -14,7 +16,8 @@ function App() {
     <Router>
       <Navbar />
       <Card />
-
+    <Login/>
+    <Signup/>
       <Switch>
 
       </Switch>
