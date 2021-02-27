cb77d7bb6b504dc8f5a5a77558e3bf3a4384ba08
Papaya Delight <jasmineteters2020@gmail.com>
2021-02-27 14:22:28 -0700

    created account pages. with account info, account update, and photo uploader

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Account from '../Components/Account';
 import Card from '../Components/Cards/';
 
 
 function Home(props) {
     return (
+      <>
+      <Account/>
         <div className="flex flex-col tablet:flex-row justify-center flex-wrap">
           <Card addToCart={props.addToCart}/>
           <Card addToCart={props.addToCart}/>
@@ -14,6 +17,7 @@ function Home(props) {
           
           
         </div>
+        </>
     )
 }
 
