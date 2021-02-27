2bd7a963a4812843750c8504849b7b3a1d14a549
Jorgen Tuck <JamesjTuck@gmail.com>
2021-02-27 12:40:26 -0700

    added missing ;

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const session = require('express-session');
@@ -23,7 +23,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
-app.use(cors())
+app.use(cors());
 // Add routes, both API and view
 app.use(routes);
 
