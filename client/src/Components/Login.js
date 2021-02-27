09150aa13aa2bf3cabfbb6377449e12a00926a0f
Parker Morgan <jpmaster13@msn.com>
2021-02-26 18:27:13 -0700

    Added a user login event to the form and changed class to className

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from 'react';
+import API from '../utils/API'
 
 const Login = () => {
+  function handleUserLogin(event) {
+    event.preventDefault();
+    let user = new FormData(event.target);
+    for (const [name, value] of user) {
+      console.log(name, value);
+    }
+    console.log(...user)
+    API.login()
+  }
+
   return (
-    <div class="flex laptop:mx-auto w-auto laptop:w-1/4 bg-pixi p-5 m-10 rounded-xl shadow-2xl justify-evenly place-items-center">
-      <div class="min-h-full place-items-center">
-        <div class="w-11/12 p-5 bg-pixi">
-          <div class="text-center w-full text-4xl ">Login</div>
-          <h1 class="pt-4 text-xl font-semibold">
-            Welcome back, <span class="font-normal">sign in to continue</span>
+    <div className="flex laptop:mx-auto w-auto laptop:w-1/4 bg-pixi p-5 m-10 rounded-xl shadow-2xl justify-evenly place-items-center">
+      <div className="min-h-full place-items-center">
+        <div className="w-11/12 p-5 bg-pixi">
+          <div className="text-center w-full text-4xl ">Login</div>
+          <h1 className="pt-4 text-xl font-semibold">
+            Welcome back,{' '}
+            <span className="font-normal">sign in to continue</span>
           </h1>
-          <form class="mt-6 login-form">
+          <form className="mt-6 login-form" onSubmit={handleUserLogin}>
             <label
               for="email"
-              class="block text-xs font-semibold text-gray-600 uppercase"
+              className="block text-xs font-semibold text-gray-600 uppercase"
             >
               E-mail
             </label>
@@ -22,12 +34,12 @@ const Login = () => {
               name="email"
               placeholder="cool.cat@email.com"
               autocomplete="email"
-              class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
+              className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
               required
             />
             <label
               for="password"
-              class="block mt-2 text-xs font-semibold text-gray-600 uppercase"
+              className="block mt-2 text-xs font-semibold text-gray-600 uppercase"
             >
               Password
             </label>
@@ -37,12 +49,12 @@ const Login = () => {
               name="password"
               placeholder="********"
               autocomplete="current-password"
-              class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
+              className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
               required
             />
             <button
               type="submit"
-              class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase  shadow-2xl p-2 px-3 mr-1 rounded bg-buttonColor hover:bg-cardColor"
+              className="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase  shadow-2xl p-2 px-3 mr-1 rounded bg-buttonColor hover:bg-cardColor"
             >
               Sign in
             </button>
@@ -51,6 +63,6 @@ const Login = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
