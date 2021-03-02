53fab4d769cc4db98c1d0c077b2543bf5b6be273
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2021-03-01 22:56:50 -0700

    adding alert for futuer payment development

--- client/src/Components/Cart/Checkout.js
+++ client/src/Components/Cart/Checkout.js
@@ -4,15 +4,33 @@ import React from 'react'
 
 export default function Checkout() {
     return (
-        <>
- 
-            <div className="container mx-auto box-border font-serif text-2xl object-center mt-12">
-                <label className="block  ">Email</label>
-                <input className="block bg-gray-200 " placeholder="" limit-value="16"></input>
+
+        <div class="flex flex-col justify-center items-center">
+
+            <div class="md:w-1/3 sm:w-full rounded-lg shadow-lg bg-white my-3">
+                <div class="flex justify-between border-b border-gray-100 px-5 py-4">
+                    <div>
+                        <i class="fas fa-exclamation-circle text-blue-500"></i>
+                        <span class="font-bold text-gray-700 text-lg">Information</span>
+                    </div>
+                   
+                </div>
+
+                <div class="px-10 py-5 text-gray-600">
+                   Soon enough you'll be able to purchased your favorite artists digital prints. Stick with us!
+            	</div>
+
+              
+            </div>
+
+            <div className="container mx-auto px-40 box-border box-shadow bg-pixi rounded-lg font-serif text-2xl mt-12 ">
+
+                <label className="block">Email</label>
+                <input className="block bg-gray-200 " placeholder="" type="email" maxlength="30" ></input>
                 <label className="block pt-5">Card Information</label>
-                <input className="block bg-gray-200" placeholder="123456789"></input>
+                <input className="block bg-gray-200" placeholder="123456789" type="text" maxlength="16"></input>
                 <label className="block pt-5">Name On Card</label>
-                <input className="block bg-gray-200" placeholder=""></input>
+                <input className="block bg-gray-200" placeholder="" type="text" maxlength="30"></input>
                 <label className="block pt-5">Country Or Region</label>
                 <select className="blockpt-3">
                     <option>United States</option>
@@ -22,11 +40,13 @@ export default function Checkout() {
                     <option>Mexico</option>
                     <option>China</option>
                 </select>
-                <button className="block mt-8 text-5xl bg-black text-white focus:bg-purple-900 rounded-lg px-12 py-3">Pay</button>
-            </div>
+                <button className="block mt-8 text-5xl bg-black text-white focus:bg-buttonColor rounded-lg px-12 py-3">Pay</button>
+          </div>
+          </div>
 
 
 
-        </>
+
+        
     )
 }
