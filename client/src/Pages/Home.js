ff63b81ec3629d7455b9784e80c29a96dabaa1f6
Papaya Delight <jasmineteters2020@gmail.com>
2021-03-03 22:16:09 -0700

    moved the cart button on the cards. added a landing page with dummy text. stylesheet for landing page. added it to homepage on app. took out css on index.css

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Card from '../Components/Cards/';
-
+import About from '../Components/About/'
 
 
 export function Home(props) {
   return (
     <>
+    <About/>
 
       <div className="flex flex-col tablet:flex-row justify-center flex-wrap">
 
