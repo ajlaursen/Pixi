23a6499e44cc60c9631d82b6ec4c38b36b166e03
Alex Laursen <ajlaursen@aol.com>
2021-03-06 13:28:11 -0500

    working on compressing images

--- client/src/Components/Account/AccountPhotoUpload.js
+++ client/src/Components/Account/AccountPhotoUpload.js
@@ -2,11 +2,28 @@ import React, { useState, useEffect } from 'react';
 import CreatableSelect from 'react-select/creatable';
 import axios from 'axios';
 
-const compress_images = require("compress-images"),
-  INPUT_path_to_your_images,
-  OUTPUT_path;
+const compress_images = require("compress-images");
 
-  INPUT_path_to_your_images = "src/img/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}"
+function resizeImage() {
+  compress_images(
+    "src/img/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}",
+    "build/img/",
+    { compress_force: false, statistic: true, autoupdate: true },
+    false,
+    { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+    { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+    { svg: { engine: "svgo", command: "--multipass" } },
+    {
+      gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] },
+    },
+    function (err, completed) {
+      if (completed === true) {
+        console.log(completed)
+        // Doing something.
+      }
+    }
+  );
+}
 
 const AccountPhotoUpload = (props) => {
   const [state, setState] = useState({ file: '', location: "https://via.placeholder.com/150" });
